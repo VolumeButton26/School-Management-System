8b461712d104de91e6176e4b467b95d96b21115b
Jarl Sarmiento <jarlkeenen@gmail.com>
2022-04-26 15:51:18 +0800

    Updated Courses Page

---
+++ components.php
@@ -0,0 +1,66 @@
+<!-- Announcements -->
+
+
+<!-- Modules -->
+<div id="modules" class="container mt-5 border">
+    <h1 class="text-dark">Modules</h1>
+    <br>
+    <div class="card">
+        <div class="card-header bg-dark">
+            <a class="card-link text-light collapse-link" data-toggle="collapse" href="#moduleOne">
+            <img class="card-img-top collapse-icon" src="icons/collapse_button.png">
+            Module 1: Basic Physics
+            </a>
+        </div>
+        <div id="moduleOne" class="collapse">
+            <div class="card-body bg-secondary">
+            <a class="card-link text-light" href="#">1.1</a>
+            <hr/>
+            <a class="card-link text-light" href="#">1.2</a>
+            <hr/>
+            <a class="card-link text-light" href="#">1.3</a>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="card">
+        <div class="card-header bg-dark">
+            <a class="card-link text-light collapse-link" data-toggle="collapse" href="#moduleTwo">
+            <img class="card-img-top collapse-icon" src="icons/collapse_button.png">
+            Module 2: Medjo Advanced Physics
+            </a>
+        </div>
+        <div id="moduleTwo" class="collapse">
+            <div class="card-body bg-secondary">
+            <a class="card-link text-light" href="#">2.1</a>
+            <hr/>
+            <a class="card-link text-light" href="#">2.2</a>
+            <hr/>
+            <a class="card-link text-light" href="#">2.3</a>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="card">
+        <div class="card-header bg-dark">
+            <a class="card-link text-light collapse-link" data-toggle="collapse" href="#moduleThree">
+            <img class="card-img-top collapse-icon" src="icons/collapse_button.png">
+            Module 3: Mas Advanced Physics
+            </a>
+        </div>
+        <div id="moduleThree" class="collapse">
+            <div class="card-body bg-secondary">
+            <a class="card-link text-light" href="#">3.1</a>
+            <hr/>
+            <a class="card-link text-light" href="#">3.2</a>
+            <hr/>
+            <a class="card-link text-light" href="#">3.3</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- People -->
+
+
+<!-- Grades -->
