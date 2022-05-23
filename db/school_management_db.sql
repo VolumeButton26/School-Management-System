09b3e8c456104a7d473168d4921d015124953609
Jarl Sarmiento <jarlkeenen@gmail.com>
2022-05-24 06:45:26 +0800

    Update school_management_db.sql

--- db/school_management_db.sql
+++ db/school_management_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2022 at 11:08 AM
+-- Generation Time: May 24, 2022 at 12:44 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -57,7 +57,9 @@ INSERT INTO `announcements` (`announcement_reference_number`, `Course_number`, `
 (16, 2, '2022-05-10 20:29:21', 'take a break for next week'),
 (17, 2, '2022-05-10 20:29:25', 'make a platformer game'),
 (18, 2, '2022-05-10 20:29:31', 'do your best'),
-(19, 2, '2022-05-11 22:16:20', 'face to face class next school year lets go');
+(19, 2, '2022-05-11 22:16:20', 'face to face class next school year lets go'),
+(20, 2, '2022-05-21 09:24:50', 'pass your project by the weekend'),
+(21, 8, '2022-05-21 12:35:52', 'We have no classes tomorrow.');
 
 -- --------------------------------------------------------
 
@@ -77,18 +79,41 @@ CREATE TABLE `assigned_courses` (
 INSERT INTO `assigned_courses` (`ID_number`, `Course_number`) VALUES
 ('s1000', 2),
 ('s1000', 3),
+('s1000', 4),
+('s1000', 5),
 ('s1000', 6),
+('s1000', 7),
+('s1000', 8),
+('s1001', 2),
+('s1001', 3),
+('s1001', 4),
+('s1001', 5),
 ('s1001', 6),
+('s1001', 7),
+('s1001', 8),
 ('s1002', 2),
+('s1002', 3),
+('s1002', 4),
+('s1002', 5),
 ('s1002', 6),
+('s1002', 7),
+('s1002', 8),
+('s1003', 2),
+('s1003', 3),
+('s1003', 4),
+('s1003', 5),
 ('s1003', 6),
 ('s1003', 7),
+('s1003', 8),
+('s1111', 2),
+('s1111', 8),
 ('t1000', 2),
 ('t1000', 3),
 ('t1000', 7),
 ('t1001', 4),
 ('t1001', 5),
-('t1002', 6);
+('t1002', 6),
+('t1111', 8);
 
 -- --------------------------------------------------------
 
@@ -108,6 +133,7 @@ CREATE TABLE `courses` (
 INSERT INTO `courses` (`Course_number`, `Course_name`) VALUES
 (4, 'AI Development'),
 (3, 'Applied Math for Games'),
+(8, 'Cloud Computing'),
 (2, 'Game Programming'),
 (7, 'Inverse Kinematics'),
 (1, 'Machine Learning'),
@@ -117,40 +143,28 @@ INSERT INTO `courses` (`Course_number`, `Course_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `grading_system_additional`
+-- Table structure for table `grading_system`
 --
 
-CREATE TABLE `grading_system_additional` (
-  `Additional_factor_ID` int(11) NOT NULL,
-  `Course_number` int(11) NOT NULL,
-  `Additional_factor_name` varchar(50) NOT NULL,
-  `Percentage` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `grading_system_main`
---
-
-CREATE TABLE `grading_system_main` (
+CREATE TABLE `grading_system` (
   `Course_number` int(11) NOT NULL,
   `Assignments_percentage` int(11) NOT NULL,
   `Quizzes_percentage` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `grading_system_main`
+-- Dumping data for table `grading_system`
 --
 
-INSERT INTO `grading_system_main` (`Course_number`, `Assignments_percentage`, `Quizzes_percentage`) VALUES
+INSERT INTO `grading_system` (`Course_number`, `Assignments_percentage`, `Quizzes_percentage`) VALUES
 (1, 60, 40),
-(2, 50, 50),
-(3, 70, 30),
+(2, 60, 40),
+(3, 60, 40),
 (4, 80, 20),
 (5, 60, 40),
 (6, 50, 50),
-(7, 60, 40);
+(7, 60, 40),
+(8, 40, 60);
 
 -- --------------------------------------------------------
 
@@ -203,9 +217,11 @@ INSERT INTO `login_information` (`ID_number`, `Password`, `Role`) VALUES
 ('s1001', '456', 'Student'),
 ('s1002', 'abc', 'Student'),
 ('s1003', 'asd', 'Student'),
+('s1111', '1111', 'Student'),
 ('t1000', 'def', 'Teacher'),
 ('t1001', 'qwe', 'Teacher'),
-('t1002', 'asd', 'Teacher');
+('t1002', 'asd', 'Teacher'),
+('t1111', '1111', 'Teacher');
 
 -- --------------------------------------------------------
 
@@ -218,8 +234,17 @@ CREATE TABLE `modules_assignments` (
   `Content` varchar(8000) NOT NULL,
   `Points` int(11) NOT NULL,
   `Due_date` datetime NOT NULL,
-  `No_of_submissions` int(11) NOT NULL
+  `No_of_submissions` int(11) NOT NULL,
+  `Answer` varchar(8000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `modules_assignments`
+--
+
+INSERT INTO `modules_assignments` (`Module_ID`, `Content`, `Points`, `Due_date`, `No_of_submissions`, `Answer`) VALUES
+(9, 'What is the formula for force?', 15, '2022-05-25 23:59:00', 2, 'F = m * a'),
+(17, 'Research at least 1 computer parts.', 10, '2022-05-25 23:59:00', 2, 'Monitor');
 
 -- --------------------------------------------------------
 
@@ -232,6 +257,17 @@ CREATE TABLE `modules_lessons` (
   `Content` varchar(8000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `modules_lessons`
+--
+
+INSERT INTO `modules_lessons` (`Module_ID`, `Content`) VALUES
+(2, 'This is the intro to physics and blahblahblah and chuchuchuc.\r\nWe will go over the physics on that things and if you know you know.'),
+(7, 'Speed force sonic flash aaaghhhhh'),
+(12, ''),
+(15, 'Cloud Computing is blablabla'),
+(16, 'The monitor is used to display the contents.');
+
 -- --------------------------------------------------------
 
 --
@@ -243,8 +279,30 @@ CREATE TABLE `modules_main` (
   `Course_number` int(11) NOT NULL,
   `Module_number` varchar(10) NOT NULL,
   `Module_name` varchar(50) NOT NULL,
-  `Module_type` varchar(20) NOT NULL
+  `Module_type` varchar(20) NOT NULL,
+  `Published` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `modules_main`
+--
+
+INSERT INTO `modules_main` (`Module_ID`, `Course_number`, `Module_number`, `Module_name`, `Module_type`, `Published`) VALUES
+(1, 2, '1', 'Basic Physics', 'Header', 1),
+(2, 2, '1.1', 'Intro to Physics', 'Lesson', 1),
+(7, 2, '2.1.1', 'Speed Force', 'Lesson', 1),
+(8, 2, '2', 'Semi-Advanced Physics', 'Header', 0),
+(9, 2, '1.2', 'Intro Assignment', 'Assignment', 1),
+(10, 2, '1.3', 'Intro Quiz', 'Quiz', 1),
+(11, 3, '1', 'Algebra', 'Header', 1),
+(12, 3, '1.1', 'Algebra for Math', 'Lesson', 1),
+(14, 8, '1', 'Introduction', 'Header', 1),
+(15, 8, '1.1', 'Introduction', 'Lesson', 1),
+(16, 8, '1.2', 'Parts of Computer', 'Lesson', 1),
+(17, 8, '1.3', 'Research of Computer Parts', 'Assignment', 1),
+(18, 8, '1.4', 'Random Quiz', 'Quiz', 1),
+(19, 8, '2.1', 'Quiz 2', 'Quiz', 1),
+(21, 8, '2', 'Second Module', 'Header', 1);
 
 -- --------------------------------------------------------
 
@@ -258,6 +316,28 @@ CREATE TABLE `modules_quizzes_choices` (
   `Choice` varchar(8000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `modules_quizzes_choices`
+--
+
+INSERT INTO `modules_quizzes_choices` (`Choice_ID`, `Question_ID`, `Choice`) VALUES
+(1, 1, 's = t / d'),
+(2, 1, 's = d / t'),
+(3, 1, 's = d * t'),
+(4, 1, 's = d - t'),
+(21, 6, 'abc'),
+(22, 6, 'def'),
+(23, 6, 'ghi'),
+(24, 6, 'jkl'),
+(25, 7, '123'),
+(26, 7, '345'),
+(27, 7, '456'),
+(28, 7, '567'),
+(41, 11, 'qwe'),
+(42, 11, 'wer'),
+(43, 11, 'asd'),
+(44, 11, 'xcv');
+
 -- --------------------------------------------------------
 
 --
@@ -270,9 +350,17 @@ CREATE TABLE `modules_quizzes_main` (
   `Points` int(11) NOT NULL,
   `Start_date` datetime NOT NULL,
   `Due_date` datetime NOT NULL,
-  `Time_limit` time NOT NULL,
   `No_of_tries` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `modules_quizzes_main`
+--
+
+INSERT INTO `modules_quizzes_main` (`Module_ID`, `Content`, `Points`, `Start_date`, `Due_date`, `No_of_tries`) VALUES
+(10, 'this is the quiz please finish on time', 1, '2022-05-28 16:00:00', '2022-05-28 17:00:00', 1),
+(18, 'Content content', 2, '2022-05-23 10:15:00', '2022-05-30 23:59:00', 2),
+(19, 'this is the first quiz of the second module lets go', 1, '2022-05-24 05:30:00', '2022-05-28 23:59:00', 3);
 
 -- --------------------------------------------------------
 
@@ -287,26 +375,23 @@ CREATE TABLE `modules_quizzes_questions` (
   `Answer` varchar(8000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `modules_quizzes_questions`
+--
+
+INSERT INTO `modules_quizzes_questions` (`Question_ID`, `Module_ID`, `Question`, `Answer`) VALUES
+(1, 10, 'What is the formula for speed?', 's = d / t'),
+(6, 18, 'What is __?', 'jkl'),
+(7, 18, 'Which is the ___?', '123'),
+(11, 19, 'first question. A', 'qwe');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `student_grades_additional`
+-- Table structure for table `student_grades`
 --
 
-CREATE TABLE `student_grades_additional` (
-  `ID_number` varchar(10) NOT NULL,
-  `Course_number` int(11) NOT NULL,
-  `Additional_factor_ID` int(11) NOT NULL,
-  `Grade` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `student_grades_main`
---
-
-CREATE TABLE `student_grades_main` (
+CREATE TABLE `student_grades` (
   `ID_number` varchar(10) NOT NULL,
   `Course_number` int(11) NOT NULL,
   `Assignments_grade` int(11) NOT NULL,
@@ -359,7 +444,8 @@ INSERT INTO `student_information` (`ID_number`, `Family_name`, `First_name`, `Mi
 ('s1000', 'Stark', 'Anthony', 'Coogler', 'M', '1', 'BSME', '1A'),
 ('s1001', 'Banner', 'Bruce', 'Willis', 'M', '3', 'BSBio', '3A'),
 ('s1002', 'Rogers', 'Steve', '', 'M', '5', 'BSPO', '5A'),
-('s1003', 'Barton', 'Clint', 'Ronin', 'M', '2', 'BSMM', '2A');
+('s1003', 'Barton', 'Clint', 'Ronin', 'M', '2', 'BSMM', '2A'),
+('s1111', 'Gallego', 'Tony', 'Jose', 'M', '2', 'BSCS', 'A');
 
 -- --------------------------------------------------------
 
@@ -377,17 +463,14 @@ CREATE TABLE `student_modules_assignments` (
   `Student_answer` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `student_modules_lessons`
+-- Dumping data for table `student_modules_assignments`
 --
 
-CREATE TABLE `student_modules_lessons` (
-  `ID_number` varchar(10) NOT NULL,
-  `Module_ID` int(11) NOT NULL,
-  `Status` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `student_modules_assignments` (`ID_number`, `Module_ID`, `Status`, `Score`, `Submission_date`, `No_of_submissions`, `Student_answer`) VALUES
+('s1000', 9, 'Submitted', 50, '2022-05-21 05:25:27', 2, 'F = m * a'),
+('s1000', 17, 'Submitted', 10, '2022-05-23 13:50:29', 2, 'Monitor'),
+('s1111', 17, 'Submitted', 10, '2022-05-23 23:44:43', 2, 'Monitor');
 
 -- --------------------------------------------------------
 
@@ -404,6 +487,15 @@ CREATE TABLE `student_modules_quizzes` (
   `No_of_tries` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `student_modules_quizzes`
+--
+
+INSERT INTO `student_modules_quizzes` (`ID_number`, `Module_ID`, `Status`, `Score`, `Submission_date`, `No_of_tries`) VALUES
+('s1000', 18, 'Submitted', 1, '2022-05-23 23:34:29', 2),
+('s1111', 18, 'Submitted', 2, '2022-05-23 23:14:03', 2),
+('s1111', 19, 'Submitted', 1, '2022-05-23 23:44:05', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -416,6 +508,17 @@ CREATE TABLE `student_modules_quiz_answers` (
   `Module_ID` int(11) NOT NULL,
   `Student_answer` varchar(8000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student_modules_quiz_answers`
+--
+
+INSERT INTO `student_modules_quiz_answers` (`ID_number`, `Question_ID`, `Module_ID`, `Student_answer`) VALUES
+('s1000', 6, 18, 'jkl'),
+('s1000', 7, 18, '456'),
+('s1111', 6, 18, 'jkl'),
+('s1111', 7, 18, '123'),
+('s1111', 11, 19, 'qwe');
 
 -- --------------------------------------------------------
 
@@ -437,7 +540,8 @@ CREATE TABLE `teacher_information` (
 INSERT INTO `teacher_information` (`ID_number`, `Family_name`, `First_name`, `Middle_name`) VALUES
 ('t1000', 'Stark', 'Howard', 'Jarvis'),
 ('t1001', 'Carter', 'Peggy', 'Linds'),
-('t1002', 'Pym', 'Hank', 'Claude');
+('t1002', 'Pym', 'Hank', 'Claude'),
+('t1111', 'Makilan', 'Julie', 'Fabillo');
 
 --
 -- Indexes for dumped tables
@@ -463,15 +567,9 @@ ALTER TABLE `courses`
   ADD UNIQUE KEY `Course_name` (`Course_name`);
 
 --
--- Indexes for table `grading_system_additional`
+-- Indexes for table `grading_system`
 --
-ALTER TABLE `grading_system_additional`
-  ADD PRIMARY KEY (`Additional_factor_ID`);
-
---
--- Indexes for table `grading_system_main`
---
-ALTER TABLE `grading_system_main`
+ALTER TABLE `grading_system`
   ADD PRIMARY KEY (`Course_number`);
 
 --
@@ -523,15 +621,9 @@ ALTER TABLE `modules_quizzes_questions`
   ADD PRIMARY KEY (`Question_ID`);
 
 --
--- Indexes for table `student_grades_additional`
+-- Indexes for table `student_grades`
 --
-ALTER TABLE `student_grades_additional`
-  ADD PRIMARY KEY (`ID_number`,`Course_number`,`Additional_factor_ID`);
-
---
--- Indexes for table `student_grades_main`
---
-ALTER TABLE `student_grades_main`
+ALTER TABLE `student_grades`
   ADD PRIMARY KEY (`ID_number`,`Course_number`);
 
 --
@@ -553,12 +645,6 @@ ALTER TABLE `student_modules_assignments`
   ADD PRIMARY KEY (`ID_number`,`Module_ID`);
 
 --
--- Indexes for table `student_modules_lessons`
---
-ALTER TABLE `student_modules_lessons`
-  ADD PRIMARY KEY (`ID_number`,`Module_ID`);
-
---
 -- Indexes for table `student_modules_quizzes`
 --
 ALTER TABLE `student_modules_quizzes`
@@ -568,7 +654,7 @@ ALTER TABLE `student_modules_quizzes`
 -- Indexes for table `student_modules_quiz_answers`
 --
 ALTER TABLE `student_modules_quiz_answers`
-  ADD PRIMARY KEY (`ID_number`);
+  ADD PRIMARY KEY (`ID_number`,`Question_ID`);
 
 --
 -- Indexes for table `teacher_information`
@@ -584,19 +670,13 @@ ALTER TABLE `teacher_information`
 -- AUTO_INCREMENT for table `announcements`
 --
 ALTER TABLE `announcements`
-  MODIFY `announcement_reference_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `announcement_reference_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `Course_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `grading_system_additional`
---
-ALTER TABLE `grading_system_additional`
-  MODIFY `Additional_factor_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Course_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `groups`
@@ -608,19 +688,19 @@ ALTER TABLE `groups`
 -- AUTO_INCREMENT for table `modules_main`
 --
 ALTER TABLE `modules_main`
-  MODIFY `Module_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Module_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `modules_quizzes_choices`
 --
 ALTER TABLE `modules_quizzes_choices`
-  MODIFY `Choice_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Choice_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `modules_quizzes_questions`
 --
 ALTER TABLE `modules_quizzes_questions`
-  MODIFY `Question_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Question_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
